5f7a5d5a6f011560271a8636a484b61921b57f84
Rukundo Nshimiyimana <117161191+rukundo0023@users.noreply.github.com>
2024-06-13 19:15:57 +0200

    Update main.dart

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,93 +1,70 @@
-// main.dart
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
-import 'product_details_screen.dart';
-
+import 'product_details.dart'; // Ensure this file is correctly named and placed in the 'lib' folder
 
 void main() {
-  runApp(MyApp());
+  runApp(ProductNavigationApp());
 }
 
-class MyApp extends StatelessWidget {
+class ProductNavigationApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Product App',
+      title: 'Product Navigation App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeScreen(), // Set HomeScreen as the initial screen
+      home: HomeScreen(),
     );
   }
 }
 
-// home_screen.dart
-import 'package:flutter/material.dart';
-import 'product_details_screen.dart'; // Import the product details screen
-
 class HomeScreen extends StatelessWidget {
-  final List<String> products = ['pixel 1', 'laptop', 'tablet' , 'pen drive']; // Example list of products
+  final List<Map<String, dynamic>> products = [
+    {
+      'name': 'Pixel 1',
+      'description': 'Pixel is the most fearful phone ever.',
+      'price': 800,
+    },
+    {
+      'name': 'Laptop',
+      'description': 'Laptop is the most productive development tool.',
+      'price': 2000,
+    },
+    {
+      'name': 'Tablet',
+      'description': 'Tablet is the most useful device ever for meeting.',
+      'price': 1500,
+    },
+    {
+      'name': 'Pen Drive',
+      'description': 'Pen Drive is a stylish phone ever.',
+      'price': 100,
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Product List'),
+        title: Text('Home'),
       ),
       body: ListView.builder(
         itemCount: products.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Text(products[index]),
+            title: Text(products[index]['name']),
+            subtitle: Text('\$${products[index]['price']}'),
             onTap: () {
-              // Navigate to product details screen when tapped
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => ProductDetailsScreen(product: products[index]),
+                  builder: (context) =>
+                      ProductDetailsScreen(product: products[index]),
                 ),
               );
             },
           );
         },
-      ),
-    );
-  }
-}
-
-// product_details_screen.dart
-import 'package:flutter/material.dart';
-
-class ProductDetailsScreen extends StatelessWidget {
-  final String product;
-
-  ProductDetailsScreen({required this.product});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Product Details'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Product Details for $product',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate back to the home screen
-                Navigator.pop(context);
-              },
-              child: Text('Back to Home'),
-            ),
-          ],
-        ),
       ),
     );
   }
